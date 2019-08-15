8e090a5da6fe7e073837ab9166959dc7460219ac
SirCleric <giovisull@gmail.com>
2019-08-15 11:31:25 +0200

    Changed naming inside area creation

--- lib/src/Util/area_creation.dart
+++ lib/src/Util/area_creation.dart
@@ -16,32 +16,32 @@ List<LayoutGridCouple> getPositionedGridCoupleList(List<List<String>> areas, Lis
 }
 
 LayoutGridCouple getPositionededGridCouple(List<List<String>> areas, LayoutGridCouple couple) {
-  LayoutGridCouple _child = couple;
+  LayoutGridCouple _couple = couple;
 
   for (int _i = 0; _i < areas.length; _i++) {
     for (int _j = 0; _j < areas[_i].length; _j++) {
-      if (areas[_i][_j] == _child.name) {
-        if (_child.col0 > _j || _child.col0 == -1) {
-          _child.col0 = _j;
+      if (areas[_i][_j] == _couple.name) {
+        if (_couple.col0 > _j || _couple.col0 == -1) {
+          _couple.col0 = _j;
         }
-        if (_child.col1 < _j + 1 || _child.col1 == -1) {
-          _child.col1 = _j + 1;
+        if (_couple.col1 < _j + 1 || _couple.col1 == -1) {
+          _couple.col1 = _j + 1;
         }
 
-        if (_child.row0 > _i || _child.row0 == -1) {
-          _child.row0 = _i;
+        if (_couple.row0 > _i || _couple.row0 == -1) {
+          _couple.row0 = _i;
         }
-        if (_child.row1 < _i + 1 || _child.row1 == -1) {
-          _child.row1 = _i + 1;
+        if (_couple.row1 < _i + 1 || _couple.row1 == -1) {
+          _couple.row1 = _i + 1;
         }
       }
     }
   }
 
   ///check that we did find the corresponding area
-  if (_child.col0 == -1) {
+  if (_couple.col0 == -1) {
     throw ("Could not find the area specified by the LayoutGridCouple, did you write it correctly?");
   }
 
-  return _child;
+  return _couple;
 }
